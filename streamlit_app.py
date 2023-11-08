529c37331f4a86d9bafeee6c81cd870d99b30992
mail8617 <42223196+mail8617@users.noreply.github.com>
2023-11-08 10:58:21 +0900

    Update streamlit_app.py
    imported URLError

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -36,6 +37,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
+streamlit.stop();
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -46,3 +48,5 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('what fruit would you like to add?','')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+# my_cur.execute("insert into fruit_load_list values ('from streamlit')")
